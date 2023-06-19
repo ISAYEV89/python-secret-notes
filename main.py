9bd9aeafc1cab6c03e3cee82fb1edce3000da87a
PROSYS\elchin.i <elchin.i@prosys.az>
2023-06-19 15:29:17 +0400

    alert

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from tkinter import *
-from PIL import ImageTk, Image
+# from PIL import ImageTk, Image
+
+from tkinter import ttk
+from tkinter.messagebox import showerror, showwarning, showinfo
+import Crypto
 
 window = Tk()
 window.title("Sectert notes")
 window.minsize(width=400, height=750)
 
-
-
-
-
-
+# Input string to be encrypted (padding to adjust length)
+input_string = "Hello World!".rjust(32)
+# Secret key (pw)
+key = b'1234567890123456'
+# Encrypt the string
+cipher = Crypto.Cipher.AES.new(key)
+encrypted_string = cipher.encrypt(input_string.encode())
+# Decrypt the encrypted string
+decrypted_string = cipher.decrypt(encrypted_string)
+# Print the original and decrypted strings
+print("Original String:", input_string)
+print("Decrypted String:", decrypted_string.decode())
 
 
 
@@ -40,6 +51,12 @@ key_enter.pack()
 def save_encrypt():
     title = title_enter.get()
     textarea = secret_enter.get("1.0", END)
+    key_info = key_enter.get()
+
+    if (textarea.strip() == "" or title.strip() == "" or key_info.strip() == ""):
+        showwarning(title="Error", message="Please enter all information")
+
+
     my_file = open("Secret note.txt", "a+")
     my_file.write(title)
     my_file.write('\n')
@@ -48,10 +65,9 @@ def save_encrypt():
     my_file.close()
     print('done')
 
+
 def decrypt():
-    print(title_enter.get())
-
-
+    textarea = secret_enter.get("1.0", END)
 
 
 
@@ -60,8 +76,5 @@ save_button.pack()
 
 decrypt_button = Button(text="Decrypt", command=decrypt)
 decrypt_button.pack()
-
-
-
 
 window.mainloop()
